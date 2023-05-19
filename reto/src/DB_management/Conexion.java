0e568e1da1af6d4bd3e0fc3ff6e3cdc1bd0262de
jonurrutia <114994790+jonurrutia@users.noreply.github.com>
2023-05-19 10:09:39 +0200

    reto finalizado 2

---
+++ reto/src/DB_management/Conexion.java
@@ -0,0 +1,49 @@
+package DB_management;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Conexion {
+        static final String SERVER_IP = "172.16.7.81";
+        static final String DB_NAME = "orcl";
+        static final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";
+        static final String DB_URL ="jdbc:oracle:thin:@"+SERVER_IP+":1521/"+ DB_NAME;
+
+
+        static final String USER = "DW31_GRUPO5";
+        static final String PASS = "Zubiri123";
+
+        private Connection conn;
+
+        private Statement st;
+        private ResultSet rs;
+
+
+        public Conexion() {
+            try {
+                Class.forName(JDBC_DRIVER);
+                System.out.println("Connection to database...");
+                conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                System.out.println("Connected.");
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
+    public Connection getConn() {
+        return conn;
+    }
+
+    public Statement getSt() {
+        return st;
+    }
+
+    public ResultSet getRs() {
+        return rs;
+    }
+}
---
+++ reto/src/Modelos/Aerolinia.java
@@ -0,0 +1,47 @@
+package Modelos;
+
+public class Aerolinia {
+    private int Id_aerolinia;
+    private String nombre;
+    private String Direccion;
+    private int telefono;
+
+    public Aerolinia(int id_aerolinia, String nombre, String direccion, int telefono) {
+        Id_aerolinia = id_aerolinia;
+        this.nombre = nombre;
+        Direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    public int getId_aerolinia() {
+        return Id_aerolinia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setId_aerolinia(int id_aerolinia) {
+        Id_aerolinia = id_aerolinia;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDireccion(String direccion) {
+        Direccion = direccion;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+}
