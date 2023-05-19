bf9feace08ac63642d18d01015b31d25a142c6b7
jonurrutia <114994790+jonurrutia@users.noreply.github.com>
2023-05-19 11:40:53 +0200

    reto concluido

---
+++ reto/src/Modelos/Seguridad.java
@@ -0,0 +1,21 @@
+package Modelos;
+
+import java.sql.Time;
+
+public class Seguridad extends Empleado{
+
+    private int Id_aeropuerto;
+
+    public Seguridad(int numemp, String nombre, String apellido, String trabajo, String hora_entrada, String hora_salida, int Sueldo, String password, int Id_aeropuerto) {
+        super(numemp, nombre, apellido, trabajo, hora_entrada, hora_salida, Sueldo, password);
+        this.Id_aeropuerto=Id_aeropuerto;
+    }
+
+    public int getId_aeropuerto() {
+        return Id_aeropuerto;
+    }
+
+    public void setId_aeropuerto(int id_aeropuerto) {
+        Id_aeropuerto = id_aeropuerto;
+    }
+}
